3d73360537e5d6e042c4fec727473b4b8ae2641a
Richard Dunks <richard@datapolitan.com>
2015-10-17 18:46:55 -0500

    adding status table for analytics

---
+++ code/create_nr_city_stats.sql
@@ -0,0 +1,12 @@
+DROP TABLE IF EXISTS niceride_mn.nr_city_stats;
+
+CREATE TABLE niceride_mn.nr_city_stats (
+    execution_time timestamp,
+    minneapolis int,
+    st_paul int,
+    falcon_heights int,
+    golden_valley int,
+    fort_snelling int
+);
+
+COMMENT ON TABLE niceride_mn.nr_city_stats IS 'Values from each run of the niceridestatus.py';
