41c40df81ea7d9e559ca77ad89f64805f783cdbf
Richard Dunks <richard@datapolitan.com>
2015-10-18 17:00:15 -0400

    prepping for aws deployment

--- code/niceride_analysis.py
+++ code/niceride_analysis.py
@@ -1,6 +1,5 @@
 '''
 Read results from database and create 24 hour graph of activity
-
 '''
 
 import psycopg2
@@ -35,28 +34,31 @@ def tweet_status(day):
     twitter.update_status(status='The past 24 hours of available #NiceRideMN bikes across #Minneapolis, #StPaul, #GoldenValley, #FalconHeights, & #FortSnelling', media_ids=[response['media_id']])
     return
 
+def main():
+    db = pgconnect['db']
+    user = pgconnect['user']
+    host = pgconnect['host']
 
-db = pgconnect['db']
-user = pgconnect['user']
-host = pgconnect['host']
+    con = psycopg2.connect(database=db, user=user, host=host, port=5432)
+    cur = con.cursor()
 
-con = psycopg2.connect(database=db, user=user, host=host, port=5432)
-cur = con.cursor()
+    df = pd.read_sql_query(open("/home/ec2-user/niceridestatus/code/summary_stats.sql").read(),con,index_col='hour_ex')
+    df.columns = ['Minneapolis','Saint Paul','Golden Valley/Falcon Heights/Fort Snelling']
 
-df = pd.read_sql_query(open("summary_stats.sql").read(),con,index_col='hour_ex')
-df.columns = ['Minneapolis','Saint Paul','Golden Valley/Falcon Heights/Fort Snelling']
+    ax = df.plot(figsize=(10,5),title="Available NiceRide Bikes for Past 24 hours")
+    ax.set_xlabel('Hour of the Day')
+    ax.set_ylabel("Number of Available NiceRide Bikes")
+    start, end = ax.get_xlim()
+    ax.xaxis.set_ticks(np.arange(start, end, 4))
+    handles, labels = ax.get_legend_handles_labels()
+    lgd = ax.legend(handles, labels, loc='upper center', bbox_to_anchor=(0.5,-0.1))
+    ax.grid('on')
+    day = (datetime.datetime.now() - datetime.timedelta(days=1)).strftime('%Y%m%d')
+    plt.savefig(day + '.png', bbox_extra_artists=(lgd,), bbox_inches='tight')
+    tweet_status(day)
 
-ax = df.plot(figsize=(10,5),title="Available NiceRide Bikes for Past 24 hours")
-ax.set_xlabel('Hour of the Day')
-ax.set_ylabel("Number of Available NiceRide Bikes")
-start, end = ax.get_xlim()
-ax.xaxis.set_ticks(np.arange(start, end, 4))
-handles, labels = ax.get_legend_handles_labels()
-lgd = ax.legend(handles, labels, loc='upper center', bbox_to_anchor=(0.5,-0.1))
-ax.grid('on')
-day = (datetime.datetime.now() - datetime.timedelta(days=1)).strftime('%Y%m%d')
-plt.savefig(day + '.png', bbox_extra_artists=(lgd,), bbox_inches='tight')
-tweet_status(day)
+    #remove file after tweeted
+    os.remove(day + '.png')
 
-#remove file after tweeted
-os.remove(day + '.png')
+if __name__ == "__main__":
+    main()
