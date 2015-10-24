1108517cb738f224540553c88eb167aca51c4660
Richard Dunks <richard@datapolitan.com>
2015-10-24 00:46:19 -0400

    adding tweet function

---
+++ code/niceride_tweet.py
@@ -0,0 +1,46 @@
+'''
+A script to provide aggregate stats for the past hour
+'''
+
+import psycopg2
+from pgconnect import pgconnect
+
+from twython import Twython, TwythonError
+
+db = pgconnect['db']
+user = pgconnect['user']
+host = pgconnect['host']
+
+def tweet_status():
+    #prep for tweet
+    CONSUMER_KEY = keys['consumer_key']
+    CONSUMER_SECRET = keys['consumer_secret']
+    ACCESS_TOKEN = keys['access_token']
+    ACCESS_TOKEN_SECRET = keys['access_token_secret']
+    twitter = Twython(CONSUMER_KEY,CONSUMER_SECRET,ACCESS_TOKEN,ACCESS_TOKEN_SECRET)
+
+    #query data for last hour
+    con = psycopg2.connect(database=db, user=user, host=host, port=5432)
+    cur = con.cursor()
+
+    cur.execute(open("~/niceridestatus/code/select_hour_stats.sql").read())
+    q = cur.fetchall()
+    r_list = []
+    for row in q:
+        r_list.append(row[0])
+
+    status_text = "In the past hour, there were an avg %s #NiceRideMN bikes avail in #Minneapolis, %s in #StPaul, and %s elsewhere" % ("{:,.0f}".format(r_list[0]),"{:,.0f}".format(r_list[1]),"{:,.0f}".format(r_list[2]))
+
+    try:
+        print status_text
+        # twitter.update_status(status=status_text)
+    except TwythonError as e:
+        print "failed to tweet" 
+        print e
+        pass
+
+def main():
+    tweet_status()
+
+if __name__ == "__main__":
+    main()
