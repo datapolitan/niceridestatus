253507b36a487c072aea4d7d162a8ff02d094a20
Richard Dunks <richard@datapolitan.com>
2015-10-24 00:49:59 -0400

    minor change

--- code/niceridestatus.py
+++ code/niceridestatus.py
@@ -7,7 +7,6 @@ import sys
 import psycopg2
 from pgconnect import pgconnect
 
-from twython import Twython, TwythonError
 from keys import keys
 
 db = pgconnect['db']
@@ -60,33 +59,6 @@ def write_to_table(city_dict):
     con.commit()
     con.close()
     return
-
-# def tweet_status(city_dict):
-#     #insert values into table 
-#     write_to_table([city_dict['execution_time'],city_dict['Minneapolis'],city_dict['Saint Paul'],city_dict['Falcon Heights'], city_dict['Golden Valley'], city_dict['Fort Snelling']])
-
-#     #prep for tweet
-#     other = city_dict['Falcon Heights'] + city_dict['Golden Valley'] + city_dict['Fort Snelling']
-#     CONSUMER_KEY = keys['consumer_key']
-#     CONSUMER_SECRET = keys['consumer_secret']
-#     ACCESS_TOKEN = keys['access_token']
-#     ACCESS_TOKEN_SECRET = keys['access_token_secret']
-#     twitter = Twython(CONSUMER_KEY,CONSUMER_SECRET,ACCESS_TOKEN,ACCESS_TOKEN_SECRET)
-
-#     status_text = ''
-
-#     now = int(datetime.datetime.now().strftime('%M')[0])
-#     if now % 2 == 0:
-#         status_text = "There are %s #NiceRideMN bikes avail in #Minneapolis, %s in #StPaul, and %s in #GoldenValley, #FalconHeights, & #FortSnelling" % ("{:,.0f}".format(city_dict['Minneapolis']),"{:,.0f}".format(city_dict['Saint Paul']),"{:,.0f}".format(other))
-#     else:
-#         status_text = "There are %s #NiceRideMN bikes avail in #Minneapolis, %s in #StPaul, and %s in #GoldenValley, #FalconHeights, & #FortSnelling." % ("{:,.0f}".format(city_dict['Minneapolis']),"{:,.0f}".format(city_dict['Saint Paul']),"{:,.0f}".format(other))
-
-#     try:
-#         twitter.update_status(status=status_text)
-#     except TwythonError as e:
-#         print "failed to tweet" 
-#         print e
-#         pass
 
 def main():
     # id_city_dict = collections.defaultdict(str)
