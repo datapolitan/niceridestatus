a2aad4015f155f775bb53a9ba2792ce7dde3d8b6
Richard Dunks <richarddunks@gmail.com>
2016-06-23 00:47:32 -0400

    fixed unnecessary code and added comments

--- code/niceridestatus.py
+++ code/niceridestatus.py
@@ -3,15 +3,12 @@ Script to tweet the status of docks
 
 '''
 
-
 import requests
 import collections
 import datetime
 
 import psycopg2
 from pgconnect import pgconnect
-
-from keys import keys
 
 db = pgconnect['db']
 user = pgconnect['user']
@@ -34,6 +31,9 @@ def get_id_city():
 
 
 def get_nr_dock(id_city_dict):
+    '''
+    Get the station status and parse into a dictionary counting available bikes by city
+    '''
     url = 'https://api-core.niceridemn.org/gbfs/en/station_status.json'
     stations = requests.get(url)
     city_dict = collections.defaultdict(int)
@@ -41,18 +41,15 @@ def get_nr_dock(id_city_dict):
     for station in stations.json()['data']['stations']:
         if station['is_installed'] == 1 and station['is_renting'] == 1 and station['is_returning'] == 1:
             city_dict[id_city_dict[station['station_id']]] += int(station['num_bikes_available'])
-    # return city_dict
+    return city_dict
 
-    ###### write data to database
-    write_to_table(city_dict)
 
-<<<<<<< HEAD
 def write_to_table(city_dict):
-    city_vals = [city_dict['execution_time'],city_dict['Minneapolis'],city_dict['Saint Paul'],city_dict['Falcon Heights'], city_dict['Golden Valley'], city_dict['Fort Snelling']]
-=======
+    '''
+    Write number of available bikes by city to the database table
+    '''
+    city_vals = [city_dict['execution_time'], city_dict['Minneapolis'], city_dict['Saint Paul'], city_dict['Falcon Heights'], city_dict['Golden Valley'], city_dict['Fort Snelling']]
 
-def write_to_table(city_vals):
->>>>>>> 55c0199eb3faf0f77074d5d249a2a392ddf31755
     con = psycopg2.connect(database=db, user=user, host=host, port=5432)
     cur = con.cursor()
     sql = "INSERT INTO niceride_mn.nr_city_stats (execution_time, minneapolis, st_paul, falcon_heights, golden_valley, fort_snelling) VALUES (%s,%s,%s,%s,%s,%s)"
@@ -61,42 +58,11 @@ def write_to_table(city_vals):
     con.close()
     return
 
-<<<<<<< HEAD
-=======
 
-def tweet_status(city_dict):
-    #insert values into table
-    write_to_table([city_dict['execution_time'], city_dict['Minneapolis'], city_dict['Saint Paul'], city_dict['Falcon Heights'], city_dict['Golden Valley'], city_dict['Fort Snelling']])
-
-    #prep for tweet
-    other = city_dict['Falcon Heights'] + city_dict['Golden Valley'] + city_dict['Fort Snelling']
-    CONSUMER_KEY = keys['consumer_key']
-    CONSUMER_SECRET = keys['consumer_secret']
-    ACCESS_TOKEN = keys['access_token']
-    ACCESS_TOKEN_SECRET = keys['access_token_secret']
-    twitter = Twython(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
-
-    status_text = ''
-
-    now = int(datetime.datetime.now().strftime('%M')[0])
-    if now % 2 == 0:
-        status_text = "%s #NiceRideMN bikes are avail in #Minneapolis, %s in #StPaul, and %s in #GoldenValley, #FalconHeights, & #FortSnelling" % ("{:,.0f}".format(city_dict['Minneapolis']), "{:,.0f}".format(city_dict['Saint Paul']), "{:,.0f}".format(other))
-    else:
-        status_text = "There are %s #NiceRideMN bikes avail in #Minneapolis, %s in #StPaul, and %s in #GoldenValley, #FalconHeights, & #FortSnelling." % ("{:,.0f}".format(city_dict['Minneapolis']), "{:,.0f}".format(city_dict['Saint Paul']), "{:,.0f}".format(other))
-
-    try:
-        twitter.update_status(status=status_text)
-    except TwythonError as e:
-        print "failed to tweet"
-        print e
-        pass
-
-
->>>>>>> 55c0199eb3faf0f77074d5d249a2a392ddf31755
 def main():
-    # id_city_dict = collections.defaultdict(str)
     icd = get_id_city()
-    get_nr_dock(icd)
+    city_dict = get_nr_dock(icd)
+    write_to_table(city_dict)
 
 if __name__ == "__main__":
     main()
