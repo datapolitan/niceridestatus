6817e7d0692646d567e95675a234bc94e674cae2
Richard Dunks <richarddunks@gmail.com>
2016-06-23 00:03:52 -0400

    update to new id name in table

--- code/update_station_loc.py
+++ code/update_station_loc.py
@@ -22,14 +22,14 @@ host = pgconnect['host']
 con = psycopg2.connect(database=db, user=user, host=host, port=5432)
 cur = con.cursor()
 
-#drop table and recreate 
+#drop table and recreate
 cur.execute(open("/home/ec2-user/niceridestatus/code/create_id_loc.sql").read())
 con.commit()
 
 sql = "INSERT INTO niceride_mn.id_loc(station_id,geom,created_at) VALUES (%s,ST_SetSRID(ST_MakePoint(%s,%s),4326),%s)"
 for station in stations.json()['data']['stations']:
     now = datetime.datetime.now()
-    cur.execute(sql,(station['station_id'], station['lon'],station['lat'],now))
+    cur.execute(sql, (station['station_id'], station['lon'], station['lat'], now))
 con.commit()
 
 con.close()
